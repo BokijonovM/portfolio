c8fa3821e1abbc5f3aac149437516a951c2282a2
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-29 17:48:57 +0100

    animation added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // LANDING PAGE ANIMATION
 var scene = document.getElementById("scene");
 var parallaxInstance = new Parallax(scene);
+
+const svgText = document.querySelectorAll("#svg-text path");
+for (let i = 0; i < svgText.length; i++) {
+  console.log(`Letter ${i} is ${svgText[i].getTotalLength()}`);
+}
