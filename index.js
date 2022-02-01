bd4e3f80ee6643d4e0ba9e3dec852f1078f7280f
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-01 08:30:58 +0100

    updated

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const svgText = document.querySelectorAll("#svg-text path");
 for (let i = 0; i < svgText.length; i++) {
   console.log(`Letter ${i} is ${svgText[i].getTotalLength()}`);
 }
+
+let navbarId = document.getElementById("navbar-image");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset < 70) {
+    navbarId.classList.remove("gradientBG");
+  }
+  if (navbarId.classList.contains("gradientBG") && window.pageYOffset > 100) {
+    return;
+  }
+  if (window.pageYOffset > 70) {
+    navbarId.classList.add("gradientBG");
+  }
+});
